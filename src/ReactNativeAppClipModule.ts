54d7af81adb698f36a6504cc87e2df4b7d7f9b6b
AlexandreFauchard <alexandre.fauchard@toolpad.fr>
2024-06-10 17:04:08 +0200

    Fix issue on Android

--- src/ReactNativeAppClipModule.ts
+++ src/ReactNativeAppClipModule.ts
@@ -1,5 +1,8 @@
 import { requireNativeModule } from "expo-modules-core";
+import { Platform } from "react-native";
 
 // It loads the native module object from the JSI or falls back to
 // the bridge module (from NativeModulesProxy) if the remote debugger is on.
-export default requireNativeModule("ReactNativeAppClip");
+const appClipModule = Platform.OS === "ios" ? requireNativeModule("ReactNativeAppClip") : {};
+
+export default appClipModule
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import { Platform } from "react-native";
 import ReactNativeAppClipModule from "./ReactNativeAppClipModule";
 
+function isAndroid () { return Platform.OS === 'android' };
+
 export function isClip(): boolean {
+  if(isAndroid()) return false;
+
   const bundleIdentifier = ReactNativeAppClipModule.getBundleIdentifier() as
     | string
     | undefined;
@@ -9,15 +14,21 @@ export function isClip(): boolean {
   return isClip;
 }
 
-export function getContainerURL(groupIdentifier: string): string {
+export function getContainerURL(groupIdentifier: string): string | undefined {
+  if(isAndroid()) return undefined;
+
   return ReactNativeAppClipModule.getContainerURL(groupIdentifier);
 }
 
-export function getBundleIdentifier(): string {
+export function getBundleIdentifier(): string | undefined {
+  if(isAndroid()) return undefined;
+
   return ReactNativeAppClipModule.getBundleIdentifier();
 }
 
 export function displayOverlay(): void {
+  if(isAndroid()) return;
+
   return ReactNativeAppClipModule.displayOverlay();
 }
 
@@ -25,12 +36,16 @@ export function setSharedCredential(
   groupIdentifier: string,
   credential: string
 ): void {
+  if(isAndroid()) return;
+
   return ReactNativeAppClipModule.setSharedCredential(
     groupIdentifier,
     credential
   );
 }
 
-export function getSharedCredential(groupIdentifier: string): string {
+export function getSharedCredential(groupIdentifier: string): string | undefined {
+  if(isAndroid()) return undefined;
+
   return ReactNativeAppClipModule.getSharedCredential(groupIdentifier);
 }
